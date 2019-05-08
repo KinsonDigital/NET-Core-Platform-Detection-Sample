1930aac5aba7e5e808bcb28ea3f18dfb7b6f62fa
Calvin Wilkinson <kinsondigital@gmail.com>
2019-05-08 11:41:52 -0400

    Add project files.

---
+++ PlatformDetection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PlatformDetection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				Console.WriteLine("The OS is Windows!");
+				Console.WriteLine($"\t{RuntimeInformation.OSDescription}");
+			}
+			else if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			{
+				Console.WriteLine("The OS is Linux!");
+				Console.WriteLine($"\t{RuntimeInformation.OSDescription}");
+			}
+			else
+			{
+				Console.WriteLine("OS Unknown. . .");
+			}
+
+
+			Console.ReadLine();
+        }
+    }
+}
